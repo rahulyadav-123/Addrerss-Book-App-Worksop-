1b6f3e6405073babe1b969cb5a8cf787e08167b3
rahul yadav <ry219613@gmail.com>
2021-12-06 01:20:29 +0530

    validate the form

---
+++ js/AddressBookform.js
@@ -0,0 +1,23 @@
+const name = document.querySelector('#name');
+const nameError = document.querySelector('.name-error');
+name.addEventListener('input', function () {
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}[ : ][A-Z]{1}[a-z]{2,}$');
+    if (nameRegex.test(name.value)) nameError.textContent = "";
+    else nameError.textContent = "Name is Incorrect";
+})
+
+const phone = document.querySelector('#phone');
+const phoneError = document.querySelector('.phone-error');
+phone.addEventListener('input', function () {
+    let phoneRegex = RegExp('^(?=.+)[0-9]{0,3}[0-9]{10}$');
+    if (phoneRegex.test(phone.value)) phoneError.textContent = "";
+    else phoneError.textContent = "Phone Number is Incorrect";
+})
+
+const address = document.querySelector('#address');
+const addressError = document.querySelector('.address-error');
+address.addEventListener('input', function () {
+    let addressRegex = RegExp('.*');
+    if (addressRegex.test(address.value)) addressError.textContent = "";
+    else addressError.textContent = "Address is Incorrect";
+})
